ffafd103ab4081c7e3b469f47dca399547e8df3e
ASHICK SHERIFF <ashicksheriff@HAT-ADMINs-MacBook-Pro.local>
2023-08-03 13:41:55 +0530

    Final

--- app/models/user_details.rb
+++ app/models/user_details.rb
@@ -3,4 +3,5 @@ class UserDetails < ApplicationRecord
       with: /\A\+\d{2}\s\d{4}\s\d{3}\s\(\d{3}\)\z/,
       message: 'Contact number should match +dd dddd ddd (ddd)'
     }
+    validates :firstname, presence: true, uniqueness: true
 end
